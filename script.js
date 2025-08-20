4e73b35fe9f1b2d68093a6b91aa643e8a96b0bce
Amin Shan <64324585+amenshan8@users.noreply.github.com>
2025-08-20 15:52:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ TITLE:Videographer | Social Media Marketer
 ORG:Adnan Kousa Media
 TEL:+31638724909
 EMAIL:adnankousa24@gmail.com
-URL:https://amenshan8.github.io/AdnanVID-MARKTER.V112/
+URL:https://adzilla.nl/
 NOTE:Capturing the moment. Crafting the brand.
 END:VCARD`;
 
